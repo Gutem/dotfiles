9b05d9d073a95545f83b3df3a58df4a0ac310e18
Gutem <gutemhc@gmail.com>
2021-02-11 18:41:03 -0300

    Add Picsum as Image source

--- dynamic_wallpaper.sh
+++ dynamic_wallpaper.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+sources=(
+"https://picsum.photos/"
+"https://source.unsplash.com/random/"
+)
+
+url=${sources[$RANDOM % ${#sources[@]} ]}
 dimension=$(xdpyinfo | awk '/dimensions/ {print $2}')
 
-wget -O /tmp/wallpaper.jpg "https://source.unsplash.com/random/${dimension}"
+if [[ ${url} == "https://picsum.photos/" ]]; then
+   dimension=$(echo $dimension | sed -e 's/x/\//g')
+   echo "Picsum"
+fi
+
+wget -q -O /tmp/wallpaper.jpg ${url}${dimension}
 
 gsettings set org.gnome.desktop.background picture-uri file:////tmp/wallpaper.jpg
